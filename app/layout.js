0b7deaa14a03dca2b953684184989a0f01ad3d25
SACHINGITHUB24 <155281782+SACHINGITHUB24@users.noreply.github.com>
2025-12-15 12:33:54 +0530

    Updated Files Pushed

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,18 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -19,11 +22,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
+      {/* <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-      </body>
+      </body> */}
+
+
+      <body className={inter.className}>
+
+{children}
+        </body>
+
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,14 +1,30 @@
 import Image from "next/image";
-import Sign from '@/SignUp/SignUp'
-import Onboarding from '@/pages/api/onboarding/page'
+import Sign from '@/components/ui/SignUp'
+import Onboarding from '@/app/onboarding/page'
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import { authOptions } from "./auth/callback"
 
-export default function Home() {
-  return (
- 
-    <Sign />,
-    <Onboarding />
+
+export default async function Home() {
+   const session = await getServerSession(authOptions)
+
+
+
    
 
+    if(session){
+      redirect("/onboarding") //or /dashboard user
+    }
  
+      return (
+    <Sign />
+
+
   );
 }
+
+
+
+
+
